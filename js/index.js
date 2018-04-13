e8e6339e07b9a0d05cdaeae47c9131d3a0bd1fe7
yangshan <yangshan@yangshans-Air.lan>
2018-04-14 00:14:37 +0800

    j解决chongtu

--- js/index.js
+++ js/index.js
@@ -3,17 +3,8 @@
 //     
 //    
 // });
-<<<<<<< HEAD
-=======
-// zhege shi v1 
 
 
-// 这里是测试
-
-
-
-// 这里是测试2
->>>>>>> 2f41bc5... ceshi2
 (function () {
   $(document).ready(function () {
     /**
